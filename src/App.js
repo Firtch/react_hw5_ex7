28c713b1bb14fb135d81be771dff89e51bfcc7c3
Firtch <firtch@gmail.com>
2021-06-05 06:41:00 +0300

    Complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {class: "off", label: "Press"};
+
+    this.press = this.press.bind(this);
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  shouldComponentUpdate() {
+    console.log("shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  press() {
+     var className = (this.state.class==="off") ? "on" : "off";
+     this.setState({ class: className});
+  }
+
+  render() {
+    console.log("render");
+    return (<div className="App">
+      <button onClick={this.press} className={this.state.class}>{this.state.label}</button>
+    </div>);
+  }
 }
 
 export default App;
